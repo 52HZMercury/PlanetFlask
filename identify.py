e248b11bf88181058d3df8df8860a7d2487d3629
52HZMercury <chenning_william@163.com>
2024-10-12 23:07:23 +0800

    add recognize

--- identify.py
+++ identify.py
@@ -22,7 +22,7 @@ app.after_request(after_request)
 app.config['UPLOAD_FOLDER'] = './static/images'
 
 # 设置允许上传的文件格式
-ALLOW_EXTENSIONS = ['jpg', 'jpeg']
+ALLOW_EXTENSIONS = ['png', 'jpg', 'jpeg']
 
 
 # 判断文件后缀是否在列表中
@@ -51,15 +51,28 @@ def uploadRecognize():
             print(prediction_top1)
             return {"data": prediction_top1, "message": "上传分析成功"}
         else:
-            return "格式错误，仅支持jpg、jpeg格式文件"
+            return "格式错误，仅支持png、jpg、jpeg格式文件"
     return {"code": '503', "data": "", "message": "仅支持post方法"}
 
 
 @app.route("/recognize")
-def recognize(file_name):
-    prediction = get_prediction(app.config['UPLOAD_FOLDER'] + '/' + file_name)
-
-    return {"data": prediction, "message": "分析成功"}
+def recognize():
+    if request.method == 'POST':
+        # 获取post过来的文件名称，从name=file参数中获取
+        filepath = request.files['file']
+        # 检测文件格式
+        if filepath and allowed_file(filepath):
+            # 对上传的图片进行推理
+            prediction_top3 = get_prediction(filepath)
+            # 只返回最高置信度的数据 获取字典的第一个键值对
+            prediction_top1 = {}
+            first_key, first_value = next(iter(prediction_top3.items()))
+            prediction_top1[first_key] = first_value
+            print(prediction_top1)
+            return {"data": prediction_top1, "message": "分析成功"}
+        else:
+            return "格式错误，仅支持png、jpg、jpeg格式文件"
+    return {"code": '503', "data": "", "message": "仅支持post方法"}
 
 # 查看图片
 @app.route("/images/<imageId>")
@@ -72,4 +85,4 @@ def get_frame(imageId):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
--- inference.py
+++ inference.py
@@ -22,13 +22,13 @@ def get_prediction(planet_img):
     #
 
     # 测试集图像预处理-RCTN：缩放裁剪、转 Tensor、归一化
-    test_transform = transforms.Compose([transforms.Resize(256),
-                                         transforms.CenterCrop(256),
-                                         transforms.ToTensor(),
-                                         transforms.Normalize(
-                                             mean=[0.485, 0.456, 0.406],
-                                             std=[0.229, 0.224, 0.225])
-                                        ])
+    test_transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(256),
+        transforms.Grayscale(num_output_channels=3),  # 确保三通道
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
     img_path = planet_img
     img_pil = Image.open(img_path)
